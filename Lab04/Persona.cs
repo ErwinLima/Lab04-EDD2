be4444832c3811ebabcd6bda1f24e3fad4e42f30
ErwinLima <ealiman@correo.url.edu.gt>
2022-10-11 00:17:15 -0600

    Persona class added

---
+++ Lab04/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public class Persona
+    {
+        public string name { get; set; }
+        public string dpi { get; set; }
+        public DateTime datebirth { get; set; }
+        public string address { get; set; }
+        public string[] companies { get; set; }       
+        
+        public string[] cartas { get; set; }
+
+        public string[] convos { get; set; }
+    }
+}
